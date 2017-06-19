49c618e31597191ba13a443a29c0cb9e408ad82c
TienDM <TienDM@TienDM>
2017-06-19 17:19:45 +0700

    xxx

--- src/main/java/tiendm/jsoup/JsoupUtil.java
+++ src/main/java/tiendm/jsoup/JsoupUtil.java
@@ -1,20 +1,13 @@
 package tiendm.jsoup;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Connection;
 import org.jsoup.Connection.Method;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class JsoupUtil {
 	public static void main(String[] args) {
